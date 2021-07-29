0f7f05f697b2f21b809b7d7f6a1a6dc2a27aef6a
Kyle Coberly <kyle.coberly@gmail.com>
2021-07-28 22:39:14 -0600

    Fix linting errors

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-var message = 'A message'
-let some_array = ["first item", 'second item', `third
-item`
-];
-var some_buttons = document.getElementsByTagName("button");
-some_button = some_buttons[  0  ]
-
-  (
-  some_array.forEach(   function  (  x  )
-{
-  console.log(    x);
+const someArray = ["first item", "second item", "third item"]
+someArray.forEach(function (x) {
+  console.log(x)
 })
-);
